677338b0caf3a510acc616764678abe431cbf216
Hjf288 <hjf288@gmail.com>
2019-04-13 02:14:29 +0200

    Add channel count and banned user count

--- src/server.py
+++ src/server.py
@@ -54,6 +54,10 @@ def main():
             ['ice_server', 'server_id']),
         'uptime': node.Gauge('mumble_uptime', 'Virtual uptime',
             ['ice_server', 'server_id']),
+        'chancount': node.Gauge('mumble_channels', 'Number of channels',
+            ['ice_server', 'server_id']),
+        'bancount': node.Gauge('mumble_users_banned', 'Number of banned users',
+            ['ice_server', 'server_id']),
     }
 
     if args.verbose:
@@ -69,12 +73,17 @@ def main():
             for server in meta.getBootedServers():
                 g_user = len(server.getUsers())
                 g_uptime = server.getUptime()
-                logger.debug('mumble_user_connected: %d' % g_user)
+                g_chancount = len(server.getChannels())
+                g_bancount = len(server.getBans())
+                logger.debug('mumble_users_connected: %d' % g_user)
                 logger.debug('mumble_uptime: %d' % g_uptime)
+                logger.debug('mumble_channels: %d' % g_chancount)
+                logger.debug('mumble_users_banned: %d' % g_bancount)
                 labels = {'server_id': server.id(), 'ice_server': ice_server}
                 gauges['users'].labels(labels).set(g_user)
                 gauges['uptime'].labels(labels).set(g_uptime)
-
+                gauges['chancount'].labels(labels).set(g_chancount)
+                gauges['bancount'].labels(labels).set(g_bancount)
             time_to_wait = args.interval - (time.time() - t1)
             if time_to_wait > 0:
                 time.sleep(time_to_wait)
